be4b173a95ad97bbdaf75a4280370c9fb3b2f7ef
Naomi Haser <naomihaser@naomis-mbp.lan>
2021-05-16 16:43:26 -0400

    edits

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import random
 import string
-import os
+
 #Using a daabase to store the long and short url connections- SQLAlchemy (python)
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db' ##urls.db is the name of the database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -27,9 +25,9 @@ class Urls(db.Model):
         self.short = short
 
 def shorten_url():
-    letters = string.ascii_lowercase + string.ascii_uppercase ##52characters
+    allletters = string.ascii_lowercase + string.ascii_uppercase ##52characters
     while True:
-        rand_letters = random.choices(letters, k=4)
+        rand_letters = random.choices(allletters, k=5)
         rand_letters = "".join(rand_letters) ##convert list to string
         short_url = Urls.query.filter_by(short=rand_letters).first() ##check shorturl does not exist
         if not short_url:
@@ -40,36 +38,32 @@ def shorten_url():
 @app.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == "POST": ##got a new url to use
-        url_received = request.form["name"]
-        found_url = Urls.query.filter_by(long=url_received).first()
+        input_url = request.form["name"]
+        found_url = Urls.query.filter_by(long=input_url).first()
 
         if found_url: #if the url is already in database
-            return redirect(url_for("display_short_url", url=found_url.short))
+            return redirect(url_for("show_shorturl", url=found_url.short))
         else: # create short url
             short_url = shorten_url()
             print(short_url)
-            new_url = Urls(url_received, short_url)
+            new_url = Urls(input_url, short_url)
             db.session.add(new_url) #add new row to database w new url
             db.session.commit() #changes to db
-            return redirect(url_for("display_short_url", url=short_url))
+            return redirect(url_for("show_shorturl", url=short_url))
     else:
         return render_template('url_page.html')
 
-@app.route('/<short_url>')
+@app.route('/<short_url>') ##redirect to the longer url using the short url
 def redirection(short_url):
     long_url = Urls.query.filter_by(short=short_url).first()
     if long_url:
         return redirect(long_url.long)
     else:
-        return f'<h1>Url doesnt exist</h1>'
+        return f'<h2>This URL does not exist</h2>'
 
 @app.route('/display/<url>')
-def display_short_url(url):
+def show_shorturl(url):
     return render_template('shorturl.html', short_url_display=url)
-
-@app.route('/all_urls')
-def display_all():
-    return render_template('all_urls.html', vals=Urls.query.all())
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
